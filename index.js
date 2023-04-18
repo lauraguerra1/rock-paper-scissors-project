038c965ea1e60e17bf6954cc322abf84674ff26d
Laura Guerra <l.garciaguerra1@gmail.com>
2023-04-18 16:23:18 -0400

    Make game playable in console

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var humanWinKeys = {
+  rock: ['scissors', 'love'],
+  paper: ['rock', 'peace'],
+  scissors: ['paper', 'love'],
+  love: ['paper', 'peace'],
+  peace: ['rock', 'scissors'],
+};
 var currentGame;
 
 function createGame(mode, player1, player2) {
@@ -12,3 +19,23 @@ function createGame(mode, player1, player2) {
     player2: player2,
   };
 }
+
+function checkWins(game, selection1, selection2) {
+  var humanSelection = selection1;
+  var computerSelection = selection2;
+ 
+  if (humanSelection === computerSelection) {
+    return `it\'s a draw`
+  } else if (humanWinKeys[humanSelection].includes(computerSelection)) {
+    return `${humanSelection} beats ${computerSelection} -- ${game.player1} wins!`
+  } else {
+    return `${computerSelection} beats ${humanSelection} -- ${game.player2} wins!`
+  }
+}
+
+function takeTurn(currentGame, selection1, selection2) {
+  var winMsg = checkWins(currentGame, selection1, selection2);
+  console.log(winMsg);
+}
+currentGame = createGame('classic', 'laura', 'computer')
+takeTurn(currentGame, 'rock', 'paper')
